6f2858be18d259bf3cf0ca0f268c0f0f77026abb
Madalina Radu <madalina.radu1@gcu.edu>
2025-07-22 11:10:23 -0700

    Debugging api errors

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,239 +1,47 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createUser = /* GraphQL */ `
-  mutation CreateUser(
-    $input: CreateUserInput!
-    $condition: ModelUserConditionInput
+export const createTodo = /* GraphQL */ `
+  mutation CreateTodo(
+    $input: CreateTodoInput!
+    $condition: ModelTodoConditionInput
   ) {
-    createUser(input: $input, condition: $condition) {
+    createTodo(input: $input, condition: $condition) {
       id
       name
-      email
-      role
-      major
-      gpa
-      affiliation
-      profileComplete
+      description
       createdAt
       updatedAt
-      owner
       __typename
     }
   }
 `;
-export const updateUser = /* GraphQL */ `
-  mutation UpdateUser(
-    $input: UpdateUserInput!
-    $condition: ModelUserConditionInput
+export const updateTodo = /* GraphQL */ `
+  mutation UpdateTodo(
+    $input: UpdateTodoInput!
+    $condition: ModelTodoConditionInput
   ) {
-    updateUser(input: $input, condition: $condition) {
+    updateTodo(input: $input, condition: $condition) {
       id
       name
-      email
-      role
-      major
-      gpa
-      affiliation
-      profileComplete
+      description
       createdAt
       updatedAt
-      owner
       __typename
     }
   }
 `;
-export const deleteUser = /* GraphQL */ `
-  mutation DeleteUser(
-    $input: DeleteUserInput!
-    $condition: ModelUserConditionInput
+export const deleteTodo = /* GraphQL */ `
+  mutation DeleteTodo(
+    $input: DeleteTodoInput!
+    $condition: ModelTodoConditionInput
   ) {
-    deleteUser(input: $input, condition: $condition) {
+    deleteTodo(input: $input, condition: $condition) {
       id
       name
-      email
-      role
-      major
-      gpa
-      affiliation
-      profileComplete
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const createProject = /* GraphQL */ `
-  mutation CreateProject(
-    $input: CreateProjectInput!
-    $condition: ModelProjectConditionInput
-  ) {
-    createProject(input: $input, condition: $condition) {
-      id
-      title
       description
-      department
-      skillsRequired
-      duration
-      applicationDeadline
-      facultyID
-      isActive
       createdAt
       updatedAt
-      __typename
-    }
-  }
-`;
-export const updateProject = /* GraphQL */ `
-  mutation UpdateProject(
-    $input: UpdateProjectInput!
-    $condition: ModelProjectConditionInput
-  ) {
-    updateProject(input: $input, condition: $condition) {
-      id
-      title
-      description
-      department
-      skillsRequired
-      duration
-      applicationDeadline
-      facultyID
-      isActive
-      createdAt
-      updatedAt
-      __typename
-    }
-  }
-`;
-export const deleteProject = /* GraphQL */ `
-  mutation DeleteProject(
-    $input: DeleteProjectInput!
-    $condition: ModelProjectConditionInput
-  ) {
-    deleteProject(input: $input, condition: $condition) {
-      id
-      title
-      description
-      department
-      skillsRequired
-      duration
-      applicationDeadline
-      facultyID
-      isActive
-      createdAt
-      updatedAt
-      __typename
-    }
-  }
-`;
-export const createApplication = /* GraphQL */ `
-  mutation CreateApplication(
-    $input: CreateApplicationInput!
-    $condition: ModelApplicationConditionInput
-  ) {
-    createApplication(input: $input, condition: $condition) {
-      id
-      studentID
-      projectID
-      statement
-      transcriptLink
-      status
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const updateApplication = /* GraphQL */ `
-  mutation UpdateApplication(
-    $input: UpdateApplicationInput!
-    $condition: ModelApplicationConditionInput
-  ) {
-    updateApplication(input: $input, condition: $condition) {
-      id
-      studentID
-      projectID
-      statement
-      transcriptLink
-      status
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const deleteApplication = /* GraphQL */ `
-  mutation DeleteApplication(
-    $input: DeleteApplicationInput!
-    $condition: ModelApplicationConditionInput
-  ) {
-    deleteApplication(input: $input, condition: $condition) {
-      id
-      studentID
-      projectID
-      statement
-      transcriptLink
-      status
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const createMessage = /* GraphQL */ `
-  mutation CreateMessage(
-    $input: CreateMessageInput!
-    $condition: ModelMessageConditionInput
-  ) {
-    createMessage(input: $input, condition: $condition) {
-      id
-      senderID
-      receiverID
-      body
-      sentAt
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const updateMessage = /* GraphQL */ `
-  mutation UpdateMessage(
-    $input: UpdateMessageInput!
-    $condition: ModelMessageConditionInput
-  ) {
-    updateMessage(input: $input, condition: $condition) {
-      id
-      senderID
-      receiverID
-      body
-      sentAt
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const deleteMessage = /* GraphQL */ `
-  mutation DeleteMessage(
-    $input: DeleteMessageInput!
-    $condition: ModelMessageConditionInput
-  ) {
-    deleteMessage(input: $input, condition: $condition) {
-      id
-      senderID
-      receiverID
-      body
-      sentAt
-      createdAt
-      updatedAt
-      owner
       __typename
     }
   }
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,165 +1,31 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getUser = /* GraphQL */ `
-  query GetUser($id: ID!) {
-    getUser(id: $id) {
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
       id
       name
-      email
-      role
-      major
-      gpa
-      affiliation
-      profileComplete
+      description
       createdAt
       updatedAt
-      owner
       __typename
     }
   }
 `;
-export const listUsers = /* GraphQL */ `
-  query ListUsers(
-    $filter: ModelUserFilterInput
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
         name
-        email
-        role
-        major
-        gpa
-        affiliation
-        profileComplete
-        createdAt
-        updatedAt
-        owner
-        __typename
-      }
-      nextToken
-      __typename
-    }
-  }
-`;
-export const getProject = /* GraphQL */ `
-  query GetProject($id: ID!) {
-    getProject(id: $id) {
-      id
-      title
-      description
-      department
-      skillsRequired
-      duration
-      applicationDeadline
-      facultyID
-      isActive
-      createdAt
-      updatedAt
-      __typename
-    }
-  }
-`;
-export const listProjects = /* GraphQL */ `
-  query ListProjects(
-    $filter: ModelProjectFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listProjects(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        title
         description
-        department
-        skillsRequired
-        duration
-        applicationDeadline
-        facultyID
-        isActive
         createdAt
         updatedAt
-        __typename
-      }
-      nextToken
-      __typename
-    }
-  }
-`;
-export const getApplication = /* GraphQL */ `
-  query GetApplication($id: ID!) {
-    getApplication(id: $id) {
-      id
-      studentID
-      projectID
-      statement
-      transcriptLink
-      status
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const listApplications = /* GraphQL */ `
-  query ListApplications(
-    $filter: ModelApplicationFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listApplications(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        studentID
-        projectID
-        statement
-        transcriptLink
-        status
-        createdAt
-        updatedAt
-        owner
-        __typename
-      }
-      nextToken
-      __typename
-    }
-  }
-`;
-export const getMessage = /* GraphQL */ `
-  query GetMessage($id: ID!) {
-    getMessage(id: $id) {
-      id
-      senderID
-      receiverID
-      body
-      sentAt
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
-export const listMessages = /* GraphQL */ `
-  query ListMessages(
-    $filter: ModelMessageFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listMessages(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        senderID
-        receiverID
-        body
-        sentAt
-        createdAt
-        updatedAt
-        owner
         __typename
       }
       nextToken
