8f97a5d191cf5296783dc36c54cda64e28f31bb2
Madalina Radu <madalina.radu1@gcu.edu>
2025-07-22 13:07:40 -0700

    Enhanced schema to support all functional requirements

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,47 +1,239 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = /* GraphQL */ `
-  mutation CreateTodo(
-    $input: CreateTodoInput!
-    $condition: ModelTodoConditionInput
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    createTodo(input: $input, condition: $condition) {
+    createUser(input: $input, condition: $condition) {
       id
       name
+      email
+      role
+      major
+      gpa
+      affiliation
+      profileComplete
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      name
+      email
+      role
+      major
+      gpa
+      affiliation
+      profileComplete
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      name
+      email
+      role
+      major
+      gpa
+      affiliation
+      profileComplete
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const createProject = /* GraphQL */ `
+  mutation CreateProject(
+    $input: CreateProjectInput!
+    $condition: ModelProjectConditionInput
+  ) {
+    createProject(input: $input, condition: $condition) {
+      id
+      title
       description
+      department
+      skillsRequired
+      duration
+      applicationDeadline
+      facultyID
+      isActive
       createdAt
       updatedAt
       __typename
     }
   }
 `;
-export const updateTodo = /* GraphQL */ `
-  mutation UpdateTodo(
-    $input: UpdateTodoInput!
-    $condition: ModelTodoConditionInput
+export const updateProject = /* GraphQL */ `
+  mutation UpdateProject(
+    $input: UpdateProjectInput!
+    $condition: ModelProjectConditionInput
   ) {
-    updateTodo(input: $input, condition: $condition) {
+    updateProject(input: $input, condition: $condition) {
       id
-      name
+      title
       description
+      department
+      skillsRequired
+      duration
+      applicationDeadline
+      facultyID
+      isActive
       createdAt
       updatedAt
       __typename
     }
   }
 `;
-export const deleteTodo = /* GraphQL */ `
-  mutation DeleteTodo(
-    $input: DeleteTodoInput!
-    $condition: ModelTodoConditionInput
+export const deleteProject = /* GraphQL */ `
+  mutation DeleteProject(
+    $input: DeleteProjectInput!
+    $condition: ModelProjectConditionInput
   ) {
-    deleteTodo(input: $input, condition: $condition) {
+    deleteProject(input: $input, condition: $condition) {
       id
-      name
+      title
       description
+      department
+      skillsRequired
+      duration
+      applicationDeadline
+      facultyID
+      isActive
       createdAt
       updatedAt
+      __typename
+    }
+  }
+`;
+export const createApplication = /* GraphQL */ `
+  mutation CreateApplication(
+    $input: CreateApplicationInput!
+    $condition: ModelApplicationConditionInput
+  ) {
+    createApplication(input: $input, condition: $condition) {
+      id
+      studentID
+      projectID
+      statement
+      transcriptLink
+      status
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const updateApplication = /* GraphQL */ `
+  mutation UpdateApplication(
+    $input: UpdateApplicationInput!
+    $condition: ModelApplicationConditionInput
+  ) {
+    updateApplication(input: $input, condition: $condition) {
+      id
+      studentID
+      projectID
+      statement
+      transcriptLink
+      status
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const deleteApplication = /* GraphQL */ `
+  mutation DeleteApplication(
+    $input: DeleteApplicationInput!
+    $condition: ModelApplicationConditionInput
+  ) {
+    deleteApplication(input: $input, condition: $condition) {
+      id
+      studentID
+      projectID
+      statement
+      transcriptLink
+      status
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const createMessage = /* GraphQL */ `
+  mutation CreateMessage(
+    $input: CreateMessageInput!
+    $condition: ModelMessageConditionInput
+  ) {
+    createMessage(input: $input, condition: $condition) {
+      id
+      senderID
+      receiverID
+      body
+      sentAt
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const updateMessage = /* GraphQL */ `
+  mutation UpdateMessage(
+    $input: UpdateMessageInput!
+    $condition: ModelMessageConditionInput
+  ) {
+    updateMessage(input: $input, condition: $condition) {
+      id
+      senderID
+      receiverID
+      body
+      sentAt
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const deleteMessage = /* GraphQL */ `
+  mutation DeleteMessage(
+    $input: DeleteMessageInput!
+    $condition: ModelMessageConditionInput
+  ) {
+    deleteMessage(input: $input, condition: $condition) {
+      id
+      senderID
+      receiverID
+      body
+      sentAt
+      createdAt
+      updatedAt
+      owner
       __typename
     }
   }
